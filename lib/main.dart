2e705ec8af686e6eb670b7ee690a8eedcc282d72
Adam Oommen Jacob <adamrubiks@gmail.com>
2021-05-11 16:14:30 +0530

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
+import 'quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +30,33 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scorekeeper = [];
+
+  //List <String> questions=['You can lead a cow down stairs but not up stairs.',
+  //'Approximately one quarter of human bones are in the feet.',
+  //'A slug\'s blood is green.',];
+
+  //List <bool> answers=[
+  //false,true,true
+  //];
+
+  void check(bool userpickedanswer) {
+    setState(() {
+    bool crctans = quizBrain.po2();
+  if (userpickedanswer==crctans) {
+    scorekeeper.add(Icon(Icons.check, color: Colors.green,));
+  } else {
+    scorekeeper.add(Icon(Icons.close,color:Colors.red,));
+  }
+
+
+    quizBrain.wee();
+  });
+
+  }
+
+  int qnno = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +69,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.po(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +93,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+               check(true);
               },
             ),
           ),
@@ -79,12 +111,14 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                check(false);
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scorekeeper,
+        )
       ],
     );
   }
---
+++ lib/question.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Question{
+
+  String questiontext;
+ bool questionanswer;
+
+Question({String q,bool a}){
+  questiontext=q;
+  questionanswer=a;
+}
+
+}
