5f13b4f5593b4e3de3f3898d68f8264990ec10e5
Adam Oommen Jacob <adamrubiks@gmail.com>
2021-05-15 09:59:30 +0530

    f

--- lib/main.dart
+++ lib/main.dart
@@ -111,6 +111,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+
                 check(false);
               },
             ),
